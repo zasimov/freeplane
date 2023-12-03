d596f0587cd6df087a3126a8ada1b307ccbe6438
Alexey Zasimov <zasimov@gmail.com>
2023-12-03 18:09:27 +0300

    hack: allow deprecated securemanager (to run under windows)

--- freeplane_framework/script/freeplane.bat
+++ freeplane_framework/script/freeplane.bat
@@ -2,4 +2,5 @@ java ^
 --add-exports=java.desktop/sun.awt.shell=ALL-UNNAMED ^
 --add-exports=java.desktop/com.sun.java.swing.plaf.windows=ALL-UNNAMED ^
 -Xmx512m -Dorg.freeplane.userfpdir="%APPDATA%\\Freeplane" ^
+-Djava.security.manager=allow ^
 -jar freeplanelauncher.jar %*
